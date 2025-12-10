fa769588d903e0e76a03a1a9f54d0e06875c3106
sot-kara <sotkarfil@ece.auth.gr>
2025-12-10 18:22:40 +0200

    Add files for second lab
    Makes a new folder for the second lab and copies the files from the first

---
+++ Second_Lab/IMAGE_DIFF_POSTERIZE.cpp
@@ -0,0 +1,38 @@
+#define WIDTH 256
+#define HEIGHT 256
+#define T1 32
+#define T2 96
+#include <stdint.h>
+
+void IMAGE_DIFF_POSTERIZE(uint8_t A [HEIGHT][WIDTH], uint8_t B[HEIGHT][WIDTH], uint8_t C[HEIGHT][WIDTH]){
+	#pragma HLS INTERFACE mode = bram port = A
+	#pragma HLS INTERFACE mode = bram port = B
+	#pragma HLS INTERFACE mode = bram port = C
+	#pragma HLS INTERFACE s_axilite port=return
+	uint8_t D; // difference of pixel values
+	int16_t temp_d;
+// Partition the arrays so more there can be more access in one iteration with loop unrolling and pipelining
+#pragma HLS ARRAY_PARTITION variable=A type=cyclic factor=64 dim=2
+#pragma HLS ARRAY_PARTITION variable=B type=cyclic factor=64 dim=2
+#pragma HLS ARRAY_PARTITION variable=C type=cyclic factor=64 dim=2
+
+	// iterate through A and B
+	for(int i = 0 ; i < HEIGHT ; i++){
+
+		#pragma HLS PIPELINE // pipeline the A and B load operations mainly
+		for(int j =0 ; j < WIDTH ; j++){
+			temp_d = A[i][j] - B[i][j];
+			D = (temp_d < 0)? -temp_d : temp_d; // get the difference of each corresponding pixels
+			if(D < T1){
+				C[i][j] = 0;
+			}
+			else if(D >= T1 && D <T2){
+				C[i][j] = 128;
+			}
+			else{
+				C[i][j] = 255;
+			}
+		}
+	}
+
+}
---
+++ Second_Lab/host.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define WIDTH 256
+#define HEIGHT 256
+#define OUTPUT_PATH "C:/Users/{USER}/Documents/ref_output.dat" // change this to your prefered path
+
+
+void IMAGE_DIFF_POSTERIZE(uint8_t A [HEIGHT][WIDTH], uint8_t B[HEIGHT][WIDTH], uint8_t C[HEIGHT][WIDTH]);
+
+int main(){
+    uint8_t A[HEIGHT][WIDTH] ;
+	uint8_t B[HEIGHT][WIDTH] ;
+
+	uint8_t ref_data[HEIGHT][WIDTH] = {};
+    // filling matrices with dummy values
+    for(int i=0; i<HEIGHT; i++){
+
+        for(int j=0; j<WIDTH; j++){
+            A[i][j] = (uint8_t) (i*j) % 256;
+            B[i][j] = (uint8_t) (i/(j+1)) % 256;
+        }
+    }
+
+	uint8_t C[HEIGHT][WIDTH] = {};
+
+	IMAGE_DIFF_POSTERIZE(A, B, C);
+
+//	for(int i = 0 ; i < HEIGHT ; i++){
+//
+//		for(int j =0 ; j < WIDTH ; j++){
+//			printf("%3d ", C[i][j]);
+//		}
+//		putchar('\n');
+//	}
+	
+    FILE *ref= fopen(OUTPUT_PATH, "r"); 
+
+    if(ref==NULL){
+
+    FILE *out = fopen(OUTPUT_PATH, "w");
+    if (!out) {
+        printf("File open error");
+        return 1;
+    }
+
+    	for(int i = 0 ; i < HEIGHT ; i++){
+
+    		for(int j =0 ; j < WIDTH ; j++){
+    			fprintf(out, "%3d ", C[i][j]);
+    		}
+    		fprintf(out, "\n");
+    	}
+    fclose(out);
+
+    printf("Reference output from C simulation written.\n");
+    return 0;
+    }
+// This part will run for the RTL part of the cosimulation
+    for (int i = 0; i < HEIGHT; i++) {
+    	for(int j = 0; j< WIDTH; j++ ){
+        if (fscanf(ref, "%3d", &ref_data[i][j]) != 1) {
+            printf("Error: reference file corrupted at index (%d,%d).\n", i,j);
+            fclose(ref);
+            remove(OUTPUT_PATH);
+            return 1;
+        }
+    	}
+    }
+    fclose(ref);
+    remove(OUTPUT_PATH);
+
+    bool match = 1;
+    for (int i = 0; i < HEIGHT; i++) {
+    	for(int j = 0; j< WIDTH; j++ ){
+    		if(C[i][j]!= ref_data[i][j]){
+    			printf("Mismatch at index (%d,%d)", i,j);
+    			match = 0;
+    		}
+    	}
+    	}
+    if(match){
+    	printf("Test PASSED!\n");
+    }
+    else{
+    	printf("Test FAILED!\n");
+    }
+  return 0;
+
+}
