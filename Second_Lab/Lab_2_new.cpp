73aaeda2acfe7dc2a28933d2b4a0028befecaf8e
SOTIRIOS KARANTZOULIS <sotkarfil@ece.auth.gr>
2025-12-18 15:01:59 +0200

    Fix IMAGE_DIFF_POSTERIZE iterations

--- Second_Lab/Lab_2_new.cpp
+++ Second_Lab/Lab_2_new.cpp
@@ -1,7 +1,7 @@
 #include <stdint.h>
 
-#define WIDTH 256
-#define HEIGHT 256
+#define WIDTH 12
+#define HEIGHT 12
 #define BUFFER_HEIGHT 3
 #define BUFFER_WIDTH 3
 #define T1 32
@@ -34,7 +34,7 @@ extern "C" {
         #pragma HLS INTERFACE s_axilite port = out bundle = control
         #pragma HLS INTERFACE s_axilite port = size bundle = control
         #pragma HLS INTERFACE s_axilite port = return bundle = control
-                
+
 
         unsigned int v1_buffer[BUFFER_HEIGHT][BUFFER_WIDTH];   // Local memory to store vector1
         unsigned int v2_buffer[BUFFER_HEIGHT][BUFFER_WIDTH];   // Local memory to store vector2
@@ -42,18 +42,17 @@ extern "C" {
         int temp_filter;
 
         // Per iteration of this loop perform BUFFER_SIZE vector addition
-        Chunk_loop: for (int i = 0; i < size; i += 1) {
+        Chunk_loop: for (int i = 0; i/WIDTH + BUFFER_HEIGHT <= HEIGHT; i += 1) {
         #pragma HLS LOOP_TRIPCOUNT min = c_len max = c_len
 
         // boundary checks
+            if ((i % WIDTH) + BUFFER_WIDTH > WIDTH) {
+                i += WIDTH - (i % WIDTH);
+            }
             if(i/WIDTH + BUFFER_HEIGHT > HEIGHT )
                 break;
-            if ((i % WIDTH) + BUFFER_WIDTH > WIDTH) {
-                i += WIDTH - (i % WIDTH)-1;
-                continue;
-            }
-            
-            
+
+
 
 
             read:    for (int j = 0; j < BUFFER_HEIGHT; j++) {
@@ -72,7 +71,7 @@ extern "C" {
                 for(int k = 0; k < BUFFER_WIDTH; k++){
             #pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
             #pragma HLS PIPELINE II = 1
-                vout_buffer[j][k] = v1_buffer[j][k] + v2_buffer[j][k];
+              //  vout_buffer[j][k] = v1_buffer[j][k] + v2_buffer[j][k];
                 vout_buffer[j][k] = Compare((uint8_t) v1_buffer[j][k], (uint8_t) v2_buffer[j][k]);
             }
         }
@@ -86,7 +85,7 @@ extern "C" {
         }
                     printf("\n");
             } */
-        
+
     // Now we check for boundary pixels and copy them directly
     if(i/WIDTH ==0){
         for(int col=0; col<BUFFER_WIDTH; col++){
@@ -94,7 +93,7 @@ extern "C" {
                 out[i + col] = vout_buffer[0][col];
         }
     }
-    
+
     if(i/WIDTH == HEIGHT - BUFFER_HEIGHT){
         for(int col=0; col<BUFFER_WIDTH; col++){
             // If last row, write last row of buffer
@@ -116,22 +115,22 @@ extern "C" {
 
     }
     // For non-boundary pixels, apply filter and write result to global memory clipping to [0,255]
-        temp_filter = 5*vout_buffer[1][1] - vout_buffer[1][2] - vout_buffer[1][0] - vout_buffer[2][1] - vout_buffer[0][1]; 
+        temp_filter = 5*vout_buffer[1][1] - vout_buffer[1][2] - vout_buffer[1][0] - vout_buffer[2][1] - vout_buffer[0][1];
         out[i + WIDTH + 1] = (uint8_t)(temp_filter<0 ? 0 : (temp_filter>255 ? 255 : temp_filter) );
 }
 
-} 
+}
 }
 uint8_t Compare(uint8_t A, uint8_t B){
     uint8_t C;
     int16_t temp_d = (int16_t) A - (int16_t) B;
-    
+
     // Note: Since temp_d is unsigned (uint8_t), it can never be < 0.
     // This logic performs a modular subtraction check.
-    uint8_t D = (temp_d < 0) ? -temp_d : temp_d; 
-    
+    uint8_t D = (temp_d < 0) ? -temp_d : temp_d;
+
     if(D < T1) C = 0;
-    else if(D < T2) C = 128; 
+    else if(D < T2) C = 128;
     else C = 255;
 
     return C;
